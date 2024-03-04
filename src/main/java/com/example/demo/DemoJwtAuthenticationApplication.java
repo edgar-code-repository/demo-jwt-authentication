ed3d5f2204ffb4046a7105a108f6bf0595d1e75c
Edgar <ecofre@gmail.com>
2024-03-03 21:27:16 -0300

    Initial commit with web, security and JWT libraries

---
+++ src/main/java/com/example/demo/DemoJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtAuthenticationApplication.class, args);
+	}
+
+}
