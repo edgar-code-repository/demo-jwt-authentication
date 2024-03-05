07a128a40de8ab7762fdd2ecb45f50f2739b6209
Edgar <ecofre@gmail.com>
2024-03-05 20:32:34 -0300

    Added util class that allows to create JWT tokens

---
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.example.demo.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class JwtUtil {
+
+    private static String SECRET_KEY = "ThisIsTheSecretSigningKeyForThisSpringRestAuthApi2024";
+
+    public static String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public static Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public static <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private static Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    private static boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private static String createToken(Map<String, Object> claims, String subject) {
+        //returns JWT token that expires within 15 minutes
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+}
