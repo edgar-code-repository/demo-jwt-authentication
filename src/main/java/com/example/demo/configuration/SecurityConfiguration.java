bba4d8aef6531438fdb4ad9a35b39724ed20a54c
Edgar <ecofre@gmail.com>
2024-03-06 23:59:50 -0300

    Added configuration that allows access to authentication endpoint

---
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.demo.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/authenticate").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/api/v1/**")
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+
+}
