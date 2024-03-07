8b42f2a65e2bab56be313ee92f50ee9b92d29531
Edgar <ecofre@gmail.com>
2024-03-06 23:29:08 -0300

    Added class implementation of UserDetails interface

---
+++ src/main/java/com/example/demo/principal/UserPrincipal.java
@@ -0,0 +1,47 @@
+package com.example.demo.principal;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }    
+	
+}
